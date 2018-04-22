e4248b76a39d28636add1b5072bbf56ebef14903
Rafael <trestini@gmail.com>
2018-04-22 11:57:30 -0300

    dynamo query and sqs publishing

--- lambdas/scheduler-warmer/index.js
+++ lambdas/scheduler-warmer/index.js
@@ -6,25 +6,69 @@ AWS.config.update({
 });
 
 const ddb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+const sqs = new AWS.SQS();
 
 exports.handler = (request, context, callback) => {
 
-  const params = {
+  const now = moment.utc();
+  const daquiA14 = moment.utc().add(14,'m');
+
+  const timeframeIni = Utils.timeframe(now);
+  const timeframeEnd = Utils.timeframe(daquiA14);
+
+  console.log(`Quering from ${timeframeIni} and ${timeframeEnd}`);
+
+  let params1 = {
+    TableName : "schedule",
+    KeyConditionExpression: "scheduleTimeframe = :ini",
     ExpressionAttributeValues: {
-      ':d1': moment.utc(),
-      ':d2': moment.utc().add(14, 'm')
-    },
-    KeyConditionExpression: 'pointInTime > :d1 and pointInTime < :d2',
-    TableName: 'schedules'
+      ":ini": timeframeIni
+    }
   };
 
-  ddb.query(params, (err, data) => {
-    if (err) {
-      console.log("Error", err);
-    } else {
-      console.log("Success", data.Items);
+  let params2 = {
+    TableName : "schedule",
+    KeyConditionExpression: "scheduleTimeframe = :end",
+    ExpressionAttributeValues: {
+      ":end": timeframeEnd
     }
+  };
+
+  const query1 = new Promise((resolve, reject) => {
+    ddb.query(params1, function(err, data) {
+      if( err ) console.error(err);
+      // console.log("Query1 items: ", data.Items);
+      err ? reject(err) : resolve(data);
+    });
   });
+
+  const query2 = new Promise((resolve, reject) => {
+    ddb.query(params2, function(err, data) {
+      if( err ) console.error(err);
+      // console.log("Query2 items: ", data.Items);
+      err ? reject(err) : resolve(data);
+    });
+  });
+
+  Promise.all([query1, query2])
+    .then(queries => {
+      let items = [];
+      queries.forEach(result => {
+        result.Items.forEach(i => items.push(i));
+      });
+
+      items
+        .filter(item => {
+          return item.currentStatus === 'COLD' && item.pointInTime > now.unix() && item.pointInTime < daquiA14.unix();
+        })
+        .forEach(item => {
+          Transition.warmIt(item);
+        });
+    })
+    .catch(err => {
+      console.error(err);
+      Utils.buildResponse(callback, 500, {err: err});
+    });
 
 };
 
@@ -52,5 +96,62 @@ const Utils = {
       errmsg = "Internal Server Error";
     }
     this.buildResponse(callback, statusCode ? statusCode : 500, {message : errmsg});
+  },
+
+  timeframe(timestamp){
+    if( !moment.isMoment(timestamp) ){
+      throw "moment.js date required";
+    }
+    const timeframePrefixIni = timestamp.format('YYYY-MM-DD_HH');
+    const timeframeIndexIni = parseInt(timestamp.minute() / 15);
+
+    const ret = `${timeframePrefixIni}-${timeframeIndexIni}`;
+    return ret;
+  }
+};
+
+const Transition = {
+  warmIt(item){
+
+    const delay = (item.pointInTime - moment.utc().unix());
+
+    console.log(`Publicando mensagem com delay de ${delay}s`);
+
+    const params = {
+      MessageBody: JSON.stringify(item), /* required */
+      QueueUrl: 'https://sqs.us-east-1.amazonaws.com/472249637553/warm-tasks', /* required */
+      DelaySeconds: delay
+    };
+
+    console.log(params);
+    sqs.sendMessage(params, function(err, data) {
+      if (err) {
+        console.error(err); // an error occurred
+      } else {
+        const params = {
+          TableName: "schedule",
+          IndexName: "scheduleId-pointInTime-index",
+          Key:{
+            "scheduleTimeframe": Utils.timeframe(moment.unix(item.pointInTime).utc()),
+            "scheduleId": item.scheduleId
+          },
+          UpdateExpression: "set currentStatus = :status, sqsMessageId = :mId",
+          ExpressionAttributeValues:{
+            ":status": "WARM",
+            ":mId": data.MessageId,
+          }
+        };
+
+        ddb.update(params, function(err, data) {
+          if (err) {
+            console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+          } else {
+            console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+          }
+        });
+
+      }
+    });
+
   }
 };
