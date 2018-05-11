4a9a7c38b5b552e322699035b2afb2828b6e4bf0
Rafael <trestini@gmail.com>
2018-05-10 21:20:15 -0300

    License information with github repo

--- lambdas/task-1minute-sqs2sns/src/support/api-gateway-request-handler.js
+++ lambdas/task-1minute-sqs2sns/src/support/api-gateway-request-handler.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 ${company}
+ * Copyright (c) 2018 delayer-aws (https://github.com/trestini/delayer-aws)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
